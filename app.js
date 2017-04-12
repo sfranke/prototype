7f086685dbde20616a76ddba39cb9d2f5d051deb
sfranke <sfr4nke@gmail.com>
2017-04-12 14:46:47 +0200

    Basic routing session and login.

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var test1 = require('./routes/test1');
 
 var app = express();
 
@@ -20,10 +22,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({resave: true, saveUninitialized: true, secret: 'ljR4546sdfgsdfgsfd34lv'}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/test1', test1);
+
+app.locals.moment = require('moment');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var bcrypt = require('bcrypt');
+var database = require('./database.js');
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  // console.log('Session:', req.session)
+  var adminUser = {
+    'name': 'admin',
+    'email': 'admin@localhost.com',
+    'password': null,
+    'permission': 'admin'
+  }
+  database.getUser(adminUser, function (error, response) {
+    if (error) {
+      console.log('Error while looking for admin user.\n' + util.inspect(error))
+      // Create admin user here!
+      adminUser.password = Math.random().toString(32).slice(2)
+      console.log('ADMIN PASSWORD: ' + adminUser.password)
+      generateHashedPassword(adminUser.password, function (error, password) {
+        if (error) console.log('error', 'Error while saving user.')
+        adminUser.password = password
+        database.saveUser(adminUser, function (error, user) {
+          if (error) console.log('error', 'Error while saving user.' + util.inspect(error))
+          if (user != null) {
+            console.log('Admin user created and saved!' + util.inspect(user))
+          }
+        })
+      })
+    }
+  })
+  serverTime();
   res.render('index', { title: 'Express' });
 });
+
+function serverTime () {
+  setInterval(function () {
+    io.emit('serverTime', moment(new Date().getTime()).format('HH:mm:ss'))
+  }, 1000)
+}
+
+function generateHashedPassword (password, callback) {
+  var hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(9))
+  // console.log('Hashed password:', hashedPassword)
+  callback(null, hashedPassword)
+};
 
 module.exports = router;
